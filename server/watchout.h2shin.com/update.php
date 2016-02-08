406ce26858ff7430be01e41c58292e2d70222ab3
Hyun-ho Shin <h2shin94@gmail.com>
2016-02-08 11:22:24 +0000

    Update Server
    Develop and tested update server script to update the database with new
    and update hazards and take expired hazards to an archive.

--- server/watchout.h2shin.com/update.php
+++ server/watchout.h2shin.com/update.php
@@ -2,23 +2,25 @@
 	require_once("includes/initialise.php");
 	$data = array();
 
-	if (!$data = json_decode(file_get_contents("php://input"))) {
+	if (!($data = json_decode(file_get_contents("php://input"), true))) {
 		exit("Provided string is not a correctly formatted JSON String");
 	}
+
+	print_r($data);
 
 	$newHazards = $data["new"];
 	$updateHazards = $data["update"];
 
-	$updateQuery = "UPDATE `hazards` SET `expires`=?s ,`acks`=?s ,`diss`=?s WHERE 1";
+	$updateQuery = "UPDATE `hazards` SET `expires`=?s ,`acks`=?s ,`diss`=?s WHERE id = ?s";
 
 	$insertQuery = "INSERT INTO `hazards` (`latitude`, `longitude`, `title`, `reported`, `expires`, `description`, `acks`, `diss`) VALUES (?s,?s,?s,?s,?s,?s,?s,?s)";
 
-	$getExpiredQuery = "SELECT * FROM `hazards` WHERE expires > NOW()";
-	$deleteExpiredQuiry = "DELETE FROM `hazards` WHERE expires > NOW()";
+	$getExpiredQuery = "SELECT * FROM `hazards` WHERE expires < NOW()";
+	$deleteExpiredQuiry = "DELETE FROM `hazards` WHERE expires < NOW()";
 
 
 	foreach ($updateHazards as $row) {
-		$db -> query($updateQuery, $row["expires"], $row["acks"], $row["diss"]);
+		$db -> query($updateQuery, $row["expires"], $row["acks"], $row["diss"], $row["id"]);
 	}
 
 	foreach ($newHazards as $row) {
@@ -31,8 +33,8 @@
 	$archive = "";
 
 	foreach ($expired as $row) {
-		$archive .= $row["latitude"] . " " .  $row["longitude"] . " " .  $row["title"] . " " .  $row["reported"] . " " .  $row["expires"] . " " .  $row["description"] . " " .  $row["acks"] . " " .  $row["diss"] . "\n";
+		$archive .= $row["id"] . ", " . $row["latitude"] . ", " .  $row["longitude"] . ", " .  $row["title"] . ", " .  $row["reported"] . ", " .  $row["expires"] . ", " .  $row["description"] . ", " .  $row["acks"] . ", " .  $row["diss"] . ";\n";
 	}
 
-	file_put_contents(SITE_ROOT."/includes/archive.txt", FILE_APPEND);
+	file_put_contents(SITE_ROOT."/archive.txt", $archive, FILE_APPEND);
 ?>
