5a8e95414b76af1e2b7dda7f0a34988bccee0b18
Emma37 <elh73@cam.ac.uk>
2016-02-14 23:15:52 +0000

    Pebble Message Testing Update
    Positive tests for all message types and negative testing added

--- Android/PebbleMessageTest.java
+++ Android/PebbleMessageTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 import com.getpebble.android.kit.util.PebbleDictionary;
@@ -11,7 +12,7 @@ public class PebbleMessageTest {
 	public void validAlertMessage() {
 		Hazard h = new Hazard();
 		int distance = 100;
-		PebbleDictionary pd = PebbleMessage.createAlert(h,distance);
+		PebbleDictionary pd = PebbleMessage.createAlert(h, distance);
 
 		assertEquals("TYPE fail in Alert", pd.getInteger(PebbleMessage.Key.TYPE.ordinal()).intValue(), PebbleMessage.Type.ALERT.ordinal());
 		assertEquals("HAZARD_ID fail in Alert", pd.getInteger(PebbleMessage.Key.HAZARD_ID.ordinal()).intValue(), h.getId());
@@ -19,4 +20,66 @@ public class PebbleMessageTest {
 		assertEquals("HAZARD_DESC fail in Alert", pd.getString(PebbleMessage.Key.HAZARD_DESC.ordinal()).toString(), h.getDescription());
 		assertEquals("HAZARD_DIST fail in Alert", pd.getInteger(PebbleMessage.Key.HAZARD_DIST.ordinal()).intValue(), distance);
 	}
+
+
+	@Test
+	public void validIgnoreMessage() {
+		Hazard h = new Hazard();
+		PebbleDictionary pd = PebbleMessage.createIgnore(h);
+
+		assertEquals("TYPE fail in Ignore", pd.getInteger(PebbleMessage.Key.TYPE.ordinal()).intValue(), PebbleMessage.Type.IGNORE.ordinal());
+		assertEquals("HAZARD_ID fail in Ignore", pd.getInteger(PebbleMessage.Key.HAZARD_ID.ordinal()).intValue(), h.getId());
+	}
+
+
+	@Test
+	public void validUpdateMessage() {
+		Hazard h = new Hazard();
+		int distance = 100;
+		PebbleDictionary pd = PebbleMessage.createUpdate(h, distance);
+
+		assertEquals("TYPE fail in Update", pd.getInteger(PebbleMessage.Key.TYPE.ordinal()).intValue(), PebbleMessage.Type.UPDATE.ordinal());
+		assertEquals("HAZARD_ID fail in Update", pd.getInteger(PebbleMessage.Key.HAZARD_ID.ordinal()).intValue(), h.getId());
+		assertEquals("HAZARD_DIST fail in Update", pd.getInteger(PebbleMessage.Key.HAZARD_DIST.ordinal()).intValue(), distance);
+	}
+	
+	
+	@Test
+	public void invalidMessage() {
+		Hazard h = new Hazard();
+		int distance = 100;
+		PebbleDictionary pd = PebbleMessage.createUpdate(h, distance);
+
+		assertFalse("TYPE fail in NegativeUpdate", pd.getInteger(PebbleMessage.Key.TYPE.ordinal()).intValue() == PebbleMessage.Type.IGNORE.ordinal());
+		assertEquals("HAZARD_ID fail in NegativeUpdate", pd.getInteger(PebbleMessage.Key.HAZARD_ID.ordinal()).intValue(), h.getId());
+		assertEquals("HAZARD_DIST fail in NegativeUpdate", pd.getInteger(PebbleMessage.Key.HAZARD_DIST.ordinal()).intValue(), distance);
+	}
+	
+	
+	@Test (expected = NullPointerException.class) // Do we want a better way to handle at this level?
+	public void nullHazard() {
+		Hazard h = null;
+		PebbleDictionary pd = PebbleMessage.createIgnore(h);
+
+		assertEquals("TYPE fail in Ignore", pd.getInteger(PebbleMessage.Key.TYPE.ordinal()).intValue(), PebbleMessage.Type.IGNORE.ordinal());
+		assertEquals("HAZARD_ID fail in Ignore", pd.getInteger(PebbleMessage.Key.HAZARD_ID.ordinal()).intValue(), h.getId());
+	}
+	
+	
+	@Test
+	public void negativeDistance() { // Do we need to consider negative distance and whether this should pass or fail?
+		Hazard h = new Hazard();
+		int distance = -100;
+		PebbleDictionary pd = PebbleMessage.createAlert(h, distance);
+
+		assertEquals("TYPE fail in Alert", pd.getInteger(PebbleMessage.Key.TYPE.ordinal()).intValue(), PebbleMessage.Type.ALERT.ordinal());
+		assertEquals("HAZARD_ID fail in Alert", pd.getInteger(PebbleMessage.Key.HAZARD_ID.ordinal()).intValue(), h.getId());
+		assertEquals("HAZARD_TYPE fail in Alert", pd.getString(PebbleMessage.Key.HAZARD_TYPE.ordinal()).toString(), h.getType());
+		assertEquals("HAZARD_DESC fail in Alert", pd.getString(PebbleMessage.Key.HAZARD_DESC.ordinal()).toString(), h.getDescription());
+		assertEquals("HAZARD_DIST fail in Alert", pd.getInteger(PebbleMessage.Key.HAZARD_DIST.ordinal()).intValue(), distance);
+	}
+
+
 }
+
+// test 
